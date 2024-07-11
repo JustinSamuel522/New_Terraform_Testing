06970b2ee8a0528629bc45e2825620cc740b8e4c
JustinSamuel522 <74522289+JustinSamuel522@users.noreply.github.com>
2024-07-11 10:49:41 -0400

    working nginx now #3

--- terraform/terraform_create.tf
+++ terraform/terraform_create.tf
@@ -4,25 +4,25 @@ provider "aws" {
   secret_key = var.aws_secret_access_key
 }
 
-# resource "aws_security_group" "allow_http" {
-#   name        = "allow_http"
-#   description = "Allow HTTP inbound traffic"
-#   vpc_id      = "vpc-0befd844e47a175ab"  # Replace with your VPC ID
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = "vpc-0befd844e47a175ab"  # Replace with your VPC ID
 
-#   ingress {
-#     from_port   = 80
-#     to_port     = 80
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]  # Allow inbound traffic from any IP address
-#   }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow inbound traffic from any IP address
+  }
 
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]  # Allow outbound traffic to any IP address
-#   }
-# }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow outbound traffic to any IP address
+  }
+}
 
 resource "aws_instance" "example" {
   ami                    = "ami-0e731c8a588258d0d" 
@@ -32,22 +32,16 @@ resource "aws_instance" "example" {
   tags = {
     Name = "terraform project"
   }
-
+  
 
   provisioner "remote-exec" {
-    inline = [
+     inline = [
       "sudo yum update -y",
-      "mkdir project",
-      "cd project",
-      "sudo yum install git -y",
-      "git clone https://github.com/JustinSamuel522/New_Terraform_Testing.git",
-      "cd ~/project/New_Terraform_Testing",
-      "sudo yum install -y nginx",
-      "sudo cp helloWorld.html /usr/share/nginx/html/index.html", # Copy helloWorld.html to nginx directory as index.html
+      "sudo yum install -y git nginx", # Install git and nginx
+      "git clone https://github.com/JustinSamuel522/New_Terraform_Testing.git /home/ec2-user/project", # Clone the repository
+      "sudo cp /home/ec2-user/project/New_Terraform_Testing/helloWorld.html /usr/share/nginx/html/index.html", # Copy helloWorld.html to nginx directory as index.html
       "sudo systemctl start nginx", # Start nginx
-      "sudo systemctl enable nginx",
-      "sudo chmod +x /home/ec2-user/setup.sh", # Make setup script executable
-      "sudo /home/ec2-user/setup.sh" # Run setup script to install Certbot and get SSL certificate
+      "sudo systemctl enable nginx"
     ]
     
     connection {
